599bb628fb261be669486c6eba44f9e260cdb063
Nikita Koliadin <qThegamEp@gmail.com>
2018-10-02 20:06:46 +0300

    Created Application class

---
+++ src/main/java/com/qthegamep/forjavarushpublication2/Application.java
@@ -0,0 +1,14 @@
+package com.qthegamep.forjavarushpublication2;
+
+import lombok.experimental.UtilityClass;
+
+/**
+ * This class is main class that start application.
+ */
+@UtilityClass
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Realization is empty!");
+    }
+}

ef01691e6dee82422402f6b18ac1842fd317069f
Nikita Koliadin <qThegamEp@gmail.com>
2018-10-03 18:47:05 +0300

    Created populateDB.sql script

---
+++ src/main/resources/populateDB.sql
@@ -0,0 +1,8 @@
+INSERT INTO PUBLIC.BOOK (NAME, PRINT_YEAR)
+VALUES ('First book', 2010),
+       ('Second book', 2011);
+
+INSERT INTO PUBLIC.AUTHOR (FIRST_NAME, SECOND_NAME, BOOK_ID)
+VALUES ('Pablo', 'Lambado', 1),
+       ('Pazo', 'Zopa', 1),
+       ('Lika', 'Vika', 2);
